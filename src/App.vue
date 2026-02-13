119eed9a326147c66a58c7b4b0c1971dac36f31c
George Lyon <130179520+grly0n@users.noreply.github.com>
2026-02-12 19:46:56 -0800

    Add Vercel analytics tracker

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Settings from './components/Settings.vue';
+import { Analytics } from '@vercel/analytics/vue';
 </script>
 
 <template>
   <Settings />
+  <Analytics />
 </template>
